849f50fc9ed829cf27b8c43403bf0d43482eccb1
Vivek241197 <kvivak952@gmail.com>
2026-02-28 20:46:18 +0530

    Onboarding changes7

--- src/main/java/com/pocketnews/repository/DeviceCategoryPreferenceRepository.java
+++ src/main/java/com/pocketnews/repository/DeviceCategoryPreferenceRepository.java
@@ -1,7 +1,9 @@
 package com.pocketnews.repository;
 
 import com.pocketnews.entity.DeviceCategoryPreference;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 
@@ -9,5 +11,7 @@ public interface DeviceCategoryPreferenceRepository extends JpaRepository<Device
 
     List<DeviceCategoryPreference> findByDeviceId(String deviceId);
 
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Transactional
     void deleteByDeviceId(String deviceId);
 }
